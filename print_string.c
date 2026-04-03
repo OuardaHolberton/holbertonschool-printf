0001b04ae1404e58504c8245fb0a7b4f04fde8ab
Rawan <rawansafouan@gmail.com>
2026-04-03 10:27:18 +0200

    adding the string print fonction

---
+++ print_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <limits.h>
+#include <stdlib.h>
+/**
+ * print_string - prints a string
+ * @str: string to print
+ *
+ * Return: number of characters printed
+ */
+int print_string(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		str = "(null)";
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+
+	return (i);
+}
