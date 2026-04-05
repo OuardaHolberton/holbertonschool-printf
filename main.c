c197efd50cdecca0c0ff2bd188066862b201f107
Rawan <rawansafouan@gmail.com>
2026-04-05 18:40:39 +0200

    adding test file for mate to test, with modified version to fit exercice standarts

--- main.c
+++ main.c
@@ -3,34 +3,50 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - teste la fonction _printf avec tous les spécificateurs autorisés
  *
- * Return: Always 0
+ * Return: 0 si tout s'exécute correctement
  */
+
 int main(void)
 {
 	int len;
 	int len2;
-	unsigned int ui;
-	void *addr;
+	char *str = NULL;
 
+	/* Test phrase simple     */
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);
-	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
+	_printf("Length: [%d, %i]\n", len, len);
+	printf("Length: [%d, %i]\n", len2, len2);
+
+	/* Test entiers positifs  */
+	_printf("Positive integers: [%d, %i]\n", 12345, 12345);
+	printf("Positive integers: [%d, %i]\n", 12345, 12345);
+
+	/* Test entiers négatifs  */
+	_printf("Negative integers: [%d, %i]\n", -762534, -762534);
+	printf("Negative integers: [%d, %i]\n", -762534, -762534);
+
+	/* Test zéro              */
+	_printf("Zero: [%d, %i]\n", 0, 0);
+	printf("Zero: [%d, %i]\n", 0, 0);
+
+	/* Test caractères        */
+	_printf("Characters: [%c, %c, %c]\n", 'H', 'e', 'y');
+	printf("Characters: [%c, %c, %c]\n", 'H', 'e', 'y');
+
+	/* Test chaînes           */
+	_printf("Strings: [%s, %s, %s]\n", "Hello", "World", "!");
+	printf("Strings: [%s, %s, %s]\n", "Hello", "World", "!");
+
+	/* Test chaîne NULL       */
+	_printf("NULL string: [%s]\n", str);
+	printf("NULL string: [%s]\n", str);
+
+	/* Test pour %%           */
+	_printf("Percent sign: [%%]\n");
+	printf("Percent sign: [%%]\n");
+
 	return (0);
 }
